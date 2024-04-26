7775cb558568a8a8c9557590f9611a1c0d292223
PaoloDno <cabrerapaolodino@gmail.com>
2024-04-27 04:14:08 +0800

    homepage added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import UserSignUp from '../components/User/UserSingUp.jsx';
 import UserLogin from '../components/User/UserLogin.jsx';
 import UserHome from './user.jsx';
+import Homepage from '../components/Home/Home.jsx';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserHome />} /> 
+        <Route path="/" element={<Homepage />} /> 
         <Route path="/user/signin" element={<UserSignUp />} />
-        <Route path="/user/login" element={<UserSignUp />} />
+        <Route path="/user/login" element={<UserLogin />} />
       </Routes>
     </Router>
   );
